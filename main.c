2295c24c102a525fd87c53ca756fda59cbfc45e8
Daruny Salazar Cardoza <dasalaza@car3s5.42barcelona.com>
2024-01-11 20:55:36 +0100

    start server

--- main.c
+++ main.c
@@ -6,14 +6,22 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 20:23:58 by dasalaza          #+#    #+#             */
-/*   Updated: 2024/01/10 20:38:29 by dasalaza         ###   ########.fr       */
+/*   Updated: 2024/01/11 20:50:49 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "include/printf/ft_printf.h"
+#include "minitalk.h"
+#include "stdio.h"
 
 int	main(void)
 {
-	ft_printf("hello world");
+	//ft_printf("hello world");
+	//ft_printf("my rocess ID is %d\n", getpid());
+	//printf("my process ID is %d\n", getpid());
+	
+	server();
+	
+
 	return (0);
 }
--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 18:15:57 by dasalaza          #+#    #+#             */
-/*   Updated: 2024/01/10 18:19:00 by dasalaza         ###   ########.fr       */
+/*   Updated: 2024/01/11 20:51:58 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 # define MINITALK_H
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <signal.h>
+#include <unistd.h>
 
+void	server();
 
 #endif
