8c2a167650cd844e9064c181c82a29985bb48cb0
Daruny Salazar Cardoza <dasalaza@car1s3.42barcelona.com>
2024-05-01 19:28:29 +0200

    remake server client

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 18:09:34 by dasalaza          #+#    #+#             */
-/*   Updated: 2024/05/01 11:37:16 by dasalaza         ###   ########.fr       */
+/*   Updated: 2024/05/01 18:19:34 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ void	check_correct_args(char **argv)
 	}
 }
 
-void	send_char(int pid, char c)
+static void	send_char(int pid, char c)
 {
 	int	i;
 	int	bit;
@@ -41,8 +41,29 @@ void	send_char(int pid, char c)
 			kill(pid, SIGUSR1);
 		else
 			kill(pid, SIGUSR2);
-		usleep(TIME_WAITING);
+		usleep(100);
 		i--;
+	}
+}
+
+//SIGUSR2 = 1
+//SIGUSR1 = 0
+void	send_length_message_to_server(int pid_server, int num)
+{
+	int	i;
+
+	i = sizeof(int) * 8 - 1;
+	if (num > 0)
+	{
+		while (i >= 0) 
+		{
+			if ((num >> i) & 1)
+				kill(pid_server, SIGUSR2);
+			else
+				kill(pid_server, SIGUSR1);
+			usleep(100);
+			i--;
+		}
 	}
 }
 
@@ -51,10 +72,38 @@ int	main(int argc, char **argv)
 	int		i;
 	char	*message_to_send;
 	int		length_message;
-	int		*count_chars;
+	int		pid_server;
+
+	i = 0;
+
+	if (argc == 3)
+	{
+		check_correct_args(argv);
+		message_to_send = argv[2];
+		length_message = ft_strlen(message_to_send);
+		pid_server = ft_atoi(argv[1]);
+		if (length_message > 0)
+			send_length_message_to_server(pid_server, length_message);
+		ft_printf("LENGTH MESSAGE => %d\n", length_message);
+		while (message_to_send[i] != '\0')
+		{
+			send_char(pid_server, message_to_send[i]);
+			i++;
+		}
+	}
+	else
+		ft_printf("number of args are incorrect!\n");
+	return (0);
+}
+
+/*
+int	main(int argc, char **argv)
+{
+	int		i;
+	char	*message_to_send;
+	int		length_message;
 
 	message_to_send = argv[2];
-	count_chars = 0;
 	i = 0;
 	if (argc == 3)
 	{
@@ -66,9 +115,9 @@ int	main(int argc, char **argv)
 			send_char(ft_atoi(argv[1]), message_to_send[i]);
 			i++;
 		}
-		//ft_printf("%d\n", count_chars);
 	}
 	else
 		ft_printf("number of args are incorrect!\n");
 	return (0);
 }
+*/
--- server.c
+++ server.c
@@ -6,47 +6,33 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 18:09:43 by dasalaza          #+#    #+#             */
-/*   Updated: 2024/05/01 11:51:02 by dasalaza         ###   ########.fr       */
+/*   Updated: 2024/05/01 18:48:57 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
-/*
-// SE DESPLAZA UN BIT A LA IZQUIERDA multiplica x 2
-// SE INTRODUCE UN 0 EN LA DERECHA
-void	handle_sigusr1(int signum)
-{
-	char	received_char;
-	
-	received_char = 0;
-	received_char <<= 1;
-}
-// SE DESPLAZA UN BIT A LA DERECHA divide entre 2
-// SE INTRODUCE UN 1 EN LA DERECHA
-void	handle_sigusr2(int signum, char received_char)
-{
-	received_char <<= 1;
-	received_char |= 1;
-}
 
-// recoge la longitd que he pasado del cliente al servidor
-// para comparar la longitud recibida con la longitud que recibo
-void	get_length_message()
+char	*print_message(int length, char c)
 {
+	char	*str;
+	int		i;
 
-}
+	str = malloc(sizeof(char *) * length);
+	if (!str)
+	{
+		ft_printf("error al crear malloc!");
+		exit(EXIT_FAILURE);
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
 
-// convertir los bits que envio del cliente hacia el servidor
-// y aqui comprobar la longitud del cliente con el servidor
-int	binary_to_char(int **ptr)
-{
-	static char received_char = 0;
-	handle_sigusr1(SIGUSR1);
-	handle_sigusr1(SIGUSR2);
+		i++;
+	}
 
 	return (0);
 }
-*/
+
 void	signals_handle(int signal)
 {
 	static int	i;
@@ -62,36 +48,21 @@ void	signals_handle(int signal)
 	if (i == 8)
 	{
 		write(1, &c, 1);
+		//ft_printf("")
 		i = 0;
 		c = 0;
 	}
 }
 
-void	received_data_from_server()
+int	main(void)
 {
-	pid_t	server_pid;
-
-	server_pid = getpid();
-	//printf("%d\n", server_pid);
-	if (!server_pid)
-	{
-		ft_printf("%s\n", "PID NOT FOUND");
-		exit(-1);
-	}
-	ft_printf("PID of Server: %d\n", server_pid);
-}
-
-int	main()
-{
-	static char received_char = 0;
-
-    // Configuracion del tratamiento de las señales SIGUSR1 y SIGUSR2.
-    //signal(SIGUSR1, handle_sigusr1);
-    //signal(SIGUSR2, handle_sigusr2);
-	//server();
+	ft_printf("Welcome to my SERVER\n");
+	ft_printf("The server is UP and running.\nThe PID of server is: %d\n", getpid());
 	signal(SIGUSR1, signals_handle);
-
+	signal(SIGUSR2, signals_handle);
+	//if ()
 	while (1)
-		usleep(100);
+		usleep(10);
+		//pause();
 	return (0);
 }
