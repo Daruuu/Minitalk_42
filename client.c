d2ac6de61d7728fd82350d7883aaf4ddc1d5c5f9
Daruny Salazar Cardoza <dasalaza@car4s5.42barcelona.com>
2024-05-03 22:33:33 +0200

    all perfect

--- client.c
+++ client.c
@@ -6,92 +6,89 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 18:09:34 by dasalaza          #+#    #+#             */
-/*   Updated: 2024/05/03 16:31:55 by dasalaza         ###   ########.fr       */
+/*   Updated: 2024/05/03 21:06:22 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
 void	check_correct_args(char **argv)
 {
-    int		i;
+	int	i;
 
-    i = 0;
-    while (argv[1][i] != '\0')
-    {
-        if (!ft_isdigit(argv[1][i]))
-        {
-            ft_printf("Invalid PID for server!\n");
-            exit(EXIT_FAILURE);
-        }
-        i++;
-    }
+	i = 0;
+	while (argv[1][i] != '\0')
+	{
+		if (!ft_isdigit(argv[1][i]))
+		{
+			ft_printf("Invalid PID for server!\n");
+			exit(EXIT_FAILURE);
+		}
+		i++;
+	}
 }
 
-// INTEGRARLO EN UN LOOP PARA ITERAR CHAR POR CHAR
 static void	send_char(int pid, char c)
 {
-    int	i;
-    int	bit;
+	int	i;
+	int	bit;
 
-    i = 7;
-    while (i >= 0)
-    {
-        bit = (c >> i) & 1;
-        if (bit == 0)
-            kill(pid, SIGUSR1);
-        else
-            kill(pid, SIGUSR2);
-        usleep(100);
-        i--;
-    }
+	i = 7;
+	while (i >= 0)
+	{
+		bit = (c >> i) & 1;
+		if (bit == 0)
+			kill(pid, SIGUSR1);
+		else
+			kill(pid, SIGUSR2);
+		usleep(100);
+		i--;
+	}
 }
 
-//SIGUSR2 = 1
-//SIGUSR1 = 0
 void	send_length_message_to_server(int pid_server, int num)
 {
-    int	i;
+	int	i;
 
-    i = sizeof(int) * 8 - 1;
-    if (num > 0)
-    {
-        while (i >= 0)
-        {
-            if ((num >> i) & 1)
-                kill(pid_server, SIGUSR2);
-            else
-                kill(pid_server, SIGUSR1);
-            usleep(100);
-            i--;
-        }
-    }
+	i = sizeof(int) * 8 - 1;
+	if (num > 0)
+	{
+		while (i >= 0)
+		{
+			if ((num >> i) & 1)
+				kill(pid_server, SIGUSR2);
+			else
+				kill(pid_server, SIGUSR1);
+			usleep(100);
+			i--;
+		}
+	}
 }
 
 int	main(int argc, char **argv)
 {
-    int		i;
-    char	*message_to_send;
-    int		length_message;
-    int		pid_server;
+	int		i;
+	char	*message_to_send;
+	int		length_message;
+	int		pid_server;
 
-    i = 0;
-    if (argc == 3)
-    {
-        check_correct_args(argv);
-        message_to_send = argv[2];
-        length_message = ft_strlen(message_to_send);
-        pid_server = ft_atoi(argv[1]);
-        if (length_message > 0)
-            send_length_message_to_server(pid_server, length_message);
-
-        ft_printf("LENGTH MESSAGE => %d\n", length_message);
-        while (message_to_send[i] != '\0')
-        {
-            send_char(pid_server, message_to_send[i]);
-            i++;
-        }
-    }
-    else
-        ft_printf("number of args are incorrect!\n");
-    return (0);
+	i = 0;
+	if (argc == 3)
+	{
+		check_correct_args(argv);
+		message_to_send = argv[2];
+		length_message = ft_strlen(message_to_send);
+		pid_server = ft_atoi(argv[1]);
+		if (length_message > 0)
+			send_length_message_to_server(pid_server, length_message);
+		ft_printf("LENGTH MESSAGE => %d\n", length_message);
+		while (message_to_send[i] != '\0')
+		{
+			send_char(pid_server, message_to_send[i]);
+			i++;
+		}
+	}
+	else
+		ft_printf("number of args are incorrect!\n");
+	return (0);
 }
--- minitalk.h
+++ minitalk.h
@@ -6,12 +6,18 @@
 /*   By: dasalaza <dasalaza@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/10 18:15:57 by dasalaza          #+#    #+#             */
-/*   Updated: 2024/05/03 16:10:31 by dasalaza         ###   ########.fr       */
+/*   Updated: 2024/05/03 21:56:13 by dasalaza         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MINITALK_H
 # define MINITALK_H
+# include <stdlib.h>
+# include <stdio.h>
+# include <signal.h>
+# include <unistd.h>
+# include "include/printf/ft_printf.h"
+# include "include/libft/libft.h"
 
 typedef struct	s_message
 {
@@ -19,12 +25,5 @@ typedef struct	s_message
 	int		length;
 	char	*content;
 }		t_message;
-
-# include <stdlib.h>
-# include <stdio.h>
-# include <signal.h>
-# include <unistd.h>
-# include "include/printf/ft_printf.h"
-# include "include/libft/libft.h"
 
 #endif
